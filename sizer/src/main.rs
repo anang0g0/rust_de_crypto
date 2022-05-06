9954076c9871283888e07c45ab23c50d9f2706fe
anang0g0 <fumumue@protonmail.com>
2022-05-06 23:04:56 +0900

    play

--- sizer/src/main.rs
+++ sizer/src/main.rs
@@ -1,3 +1,11 @@
+fn take(v: Vec<i32>) -> Vec<i32> {
+    return v;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut v = vec![1, 2, 3];
+
+    v = take(v);
+
+    println!("v[0] is: {}", v[0]);
 }
---
+++ sizer/target/debug/sizer.d
@@ -0,0 +1,1 @@
+/home/test/fugo/sizer/target/debug/sizer: /home/test/fugo/sizer/src/main.rs
