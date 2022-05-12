9665e31ce018e332f1f87f1131ec3a7a8bfc8132
anang0g0 <fumumue@protonmail.com>
2022-05-12 23:15:42 +0900

    modified

--- csprng/src/main.rs
+++ csprng/src/main.rs
@@ -1,4 +1,3 @@
-//use rand_chacha;
 use rand_chacha::rand_core::SeedableRng;
 use rand_chacha::rand_core::RngCore;
 use rand::rngs::OsRng;
@@ -12,9 +11,9 @@ fn generate_random_numbers_with_a_seed(seed : u64) {
 }
 
 fn main(){
-generate_random_numbers_with_a_seed(200); 
-//Result : -416273517
-let prng = ChaCha20Core::from_entropy();
-let mut reseeding_rng = ReseedingRng::new(prng, 0, OsRng); //Reseeding
-println!("Random number: {}", reseeding_rng.gen::<u64>());
-}
+    generate_random_numbers_with_a_seed(200); 
+    //Result : -416273517
+    let prng = ChaCha20Core::from_entropy();
+    let mut reseeding_rng = ReseedingRng::new(prng, 0, OsRng); //Reseeding
+    println!("Random number: {}", reseeding_rng.gen::<u64>());
+    }
