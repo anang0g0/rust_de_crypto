b68334bfad7199487a41527dec7c3955b06b81fa
anang0g0 <fumumue@protonmail.com>
2022-06-01 20:27:07 +0900

    bugfix

--- v2/src/main.rs
+++ v2/src/main.rs
@@ -11,43 +11,6 @@ use rand_chacha::ChaCha20Core;
 
 
 
-/*
-    Fisher-Yates shuffle による方法
-    配列の要素をランダムシャッフルする
-*/
-fn r_shuffule(mut array: [u8; 256], size: u16, seed:u64) -> [u8; 256] {
-    let _i: usize;
-    let mut a: usize;
-    let mut b: usize;
-    let seed2: [u8; 32] = [17;32]; 
-    let mut rng2: rand::rngs::StdRng = rand::SeedableRng::from_seed(seed2);
-    //let seed: u64 = 1;
-    //let mut rng = rand_chacha::ChaCha20Rng::seed_from_u64(seed);
-    //let mut _c: usize;
-    //let mut it: usize; // genはRng traitに定義されている
-    //let be;
-    
-    //Result : -416273517
-    let mut rng = rand_chacha::ChaCha20Rng::seed_from_u64(seed);
-  
-
-
-    for _i in (1..size).rev() {
-        a = (_i) as usize;
-        //_c = (rng.gen_range(1..256)) as usize; //暗号理論的に安全だが初期値が小さい、再現あり
-        //b=c as usize;
-        let be = rng.gen_range(1..256); // 32バイトシードで再現あり
-        let it =(rand::thread_rng().gen_range(1..256) % _i) as usize; //毎回変わる        
-    
-        b = be; //be&_c;
-        // ソートするキーの型
-        (array[a], array[b]) = (array[b], array[a])
-    }
-
-    array
-}
-
-
 
 /*
     Fisher-Yates shuffle による方法
@@ -71,37 +34,6 @@ fn random_shuffule(mut array: [u8; 256], size: u16, seed:u64) -> [u8; 256] {
     array
 }
 
-
-/*
-    Fisher-Yates shuffle による方法
-    配列の要素をランダムシャッフルする
-*/
-fn random_shuffule2(mut array: [u8; 256], size: u16) -> [u8; 256] {
-    let _i: usize;
-    let mut a: usize;
-    let mut b: usize;
-    let seed2: [u8; 32] = [17;32]; 
-    let mut rng2: rand::rngs::StdRng = rand::SeedableRng::from_seed(seed2);
-    //let seed: u64 = 1;
-    //let mut rng = rand_chacha::ChaCha20Rng::seed_from_u64(seed);
-    //let mut _c: usize;
-    //let mut it: usize; // genはRng traitに定義されている
-    //let be;
-
-    for _i in (0..size).rev() {
-        a = (_i) as usize;
-        //_c = (rng.gen_range(1..256)) as usize; //暗号理論的に安全だが初期値が小さい、再現あり
-        //b=c as usize;
-        //let be = rng2.gen::<u8>() as usize; // 32バイトシードで再現あり
-        let b:usize = (rng2.gen::<u8>()% _i as u8) as usize; // 32バイトシードで再現あり
-        //(rand::thread_rng().gen_range(1..256) % _i) as usize; //毎回変わる        
-        //b = it; //be&_c;
-        // ソートするキーの型
-        (array[a], array[b]) = (array[b], array[a])
-    }
-
-    array
-}
 
 fn enc(data: &String, a: &[u8; 256],mat:&Array2<u8>) -> String {
     /*
