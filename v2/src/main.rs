acdff157884192316c1db5b7f320bb0f835f1bd0
anang0g0 <fumumue@protonmail.com>
2022-06-05 13:39:26 +0900

    worning

--- v2/src/main.rs
+++ v2/src/main.rs
@@ -1,16 +1,11 @@
 #![allow(non_snake_case)]
-
 use base64::{decode, encode};
-use rand::prelude::*;
-use rand::rngs::adapter::ReseedingRng;
-use rand::rngs::OsRng;
+//use rand::prelude::*;
+//use rand::rngs::adapter::ReseedingRng;
 use rand::{Rng, SeedableRng};
-use rand_chacha::rand_core::RngCore;
-use rand_chacha::ChaCha20Core;
+//use rand_chacha::ChaCha20Core;
 use std::{process::exit, str};
-//use sha3::{Digest, Sha3_256};
 use sha3::{Digest, Keccak256};
-use hex_literal::hex;
 use sha3::Sha3_256;
 use std::io::Write;
 
@@ -21,15 +16,15 @@ use std::io::Write;
 */
 fn random_shuffule(mut array: [u8; 256], size: u16, seed: u64) -> [u8; 256] {
     let _i: usize;
-    let mut a: usize;
-    let mut b: usize;
+    let mut _a: usize;
+    let mut _b: usize;
     //let seed: u64 = 1;
     let mut rng = rand_chacha::ChaCha20Rng::seed_from_u64(seed);
 
     for _i in (1..size).rev() {
-        a = (_i) as usize;
-        let b = rng.gen::<u8>() % _i as u8; // 32バイトシードで再現あり
-        (array[a], array[b as usize]) = (array[b as usize], array[a])
+        _a = (_i) as usize;
+        let _b = rng.gen::<u8>() % _i as u8; // 32バイトシードで再現あり
+        (array[_a], array[_b as usize]) = (array[_b as usize], array[_a])
     }
 
     array
@@ -49,17 +44,10 @@ fn s2b(test:&str) -> &[u8]{
     
 fn b2s(bytes:&[u8]) -> String{
     
-        let res = bytes.iter().map(|&s| s as char).collect::<String>();
-        let converted: String = String::from_utf8(bytes.to_vec()).unwrap();
-        let mut be:String;
-        //println!("{}",res);
-     
-        //println!("{}", converted);
-        
-    //    let mut it:&str=&converted;
-        //&it=&be;
-    
-        converted
+        //let res = bytes.iter().map(|&s| s as char).collect::<String>();
+        let _converted: String = String::from_utf8(bytes.to_vec()).unwrap();
+
+        _converted
     }
     
 fn S2str(data: &String) -> &str {
@@ -77,24 +65,24 @@ fn pappy(a:[u8;256])-> [u8;256]{
     let mut count =0;
     
     hasher.update(a);
-    let result = hasher.finalize();
+    let _result = hasher.finalize();
     
     let mut buf:[u8;32]=[0;32];
     let mut u2:[u8;256]=[0;256];
     
-    for i in 0..32{
-        buf[i]=a[i];
+    for _i in 0..32{
+        buf[_i]=a[_i];
     }
-    for i in 0..8{
+    for _i in 0..8{
     let mut hasher = Sha3_256::default();
     //me=hasher.clone();
     hasher.update(buf);
         // read hash digest
         let result = hasher.finalize();
     
-    for i in 0..32{
-        buf[i]^=result[i];
-        u2[count]=buf[i];
+    for _i in 0..32{
+        buf[_i]^=result[_i];
+        u2[count]=buf[_i];
         //print!("{},",result[i]);
         count=count+1;
     }
@@ -155,14 +143,14 @@ fn enc(data: &String, a: &[u8; 256], mat: &Array2<u8>) -> String {
 
     result=pappy(result);
     println!("{:?}",result);
-    let mut t=0;
-    for k in 0..16 {
 
-        for i in 0..j {
+    for _k in 0..16 {
+
+        for _i in 0..j {
             //buf[i]^=result[i];
-            buf[i] = S_BOX[((buf[i] % 16) + (buf[i] >> 4) * 16) as usize];
-            me[i] = a[buf[i] as usize] as u8;
-            buf[i] = mat[[ a[(16 * k + i) % cycle] as usize, me[i] as usize]] as u8;
+            buf[_i] = S_BOX[((buf[_i] % 16) + (buf[_i] >> 4) * 16) as usize];
+            me[_i] = a[buf[_i] as usize] as u8;
+            buf[_i] = mat[[ a[(16 * _k + _i) % cycle] as usize, me[_i] as usize]] as u8;
 
         }
     }
@@ -219,8 +207,9 @@ fn dec(encoded: &String, a: &[u8; 256], mat: &Array2<u8>) -> String {
         inv_P[a[i as usize] as usize] = i as usize;
     }
     let l = decoded.len();
-    let size: usize = 32;
-    let mut result:[u8;256]=[17;256];
+    let _size: usize = 32;
+    let mut result:[u8;256]=[0;256];
+    
     result=pappy(result);
     //println!("{:?}",result);
 
@@ -257,7 +246,7 @@ fn dec(encoded: &String, a: &[u8; 256], mat: &Array2<u8>) -> String {
 fn hmac(message:String,key:[u8;32])->Vec<u8>{
     let ipad:[u8;32]=[0x36;32];
     let opad:[u8;32]=[0x5c;32];
-    let mut m:&[u8]=message.as_bytes();
+    let m:&[u8]=message.as_bytes();
     let mut hasher=Keccak256::default();
     let mut k1:Vec<u8>=key.to_vec();
     let mut k2:Vec<u8>=key.to_vec();
@@ -267,7 +256,7 @@ fn hmac(message:String,key:[u8;32])->Vec<u8>{
     }
     let mut K1:Vec<u8>=vec![0];
     let mut K2:Vec<u8>=vec![0];
-    let mut K3:String="".to_string();
+
     K1.write(&k1).unwrap();
     K2.write(&k2).unwrap();
     K2.write(m).unwrap();
@@ -317,23 +306,23 @@ fn main() {
 
     let seed2: [u8; 32] = [17; 32];
     let mut rng2: rand::rngs::StdRng = rand::SeedableRng::from_seed(seed2);
-    let mut seedA: u64 = 1234567890;
+    let seedA: u64 = 1234567890;
     let seedB: u64 = 1234567890;
-    let mut rngA = rand_chacha::ChaCha20Rng::seed_from_u64(seedA);
-    let mut rngB = rand_chacha::ChaCha20Rng::seed_from_u64(seedB);
+    let mut _rngA = rand_chacha::ChaCha20Rng::seed_from_u64(seedA);
+    let mut _rngB = rand_chacha::ChaCha20Rng::seed_from_u64(seedB);
     let test:&str="kotobahairanai";
     let bytes:&[u8]=test.as_bytes();
-    let mut seed: u64 = rng2.gen_range(1..256);
+    let mut _seed: u64 = rng2.gen_range(1..256);
 
     println!("{:?}",bytes);
     s2b(test);
     S2str(&b2s(bytes));
     //exit(1);
     let l=bytes.len();
-    for i in 0..l{
-        sk[i]=bytes[i];
+    for _i in 0..l{
+        sk[_i]=bytes[_i];
     }
-    for i in 0..10{
+    for _i in 0..10{
     sk=pappy(sk);
     //println!("{:?}",sk);
     }
@@ -347,32 +336,32 @@ fn main() {
     */
     //exit(1);
     let l=bytes.len();
-    for i in 0..l{
-        sk[i]=bytes[i];
+    for _i in 0..l{
+        sk[_i]=bytes[_i];
     }
-    for i in 0..10{
+    for _i in 0..10{
     sk=pappy(sk);
     //println!("{:?}",sk);
     }
     //exit(1);
 
-    for j in 0..256 {
+    for _j in 0..256 {
         for _i in 0..256 {
             sk[_i] = _i as u8;
         }
-        let seed=rng2.gen::<u64>();
+        let _seed=rng2.gen::<u64>();
         //rng2.gen::<u64>(); // 32バイトシードで再現あり
-        sk = random_shuffule(sk, 256, seed);
-        for k in 0..256 {
-            mat[[j, k]] = sk[k];
+        sk = random_shuffule(sk, 256, _seed);
+        for _k in 0..256 {
+            mat[[_j, _k]] = sk[_k];
             //print!("{}, ", mat[[j, k]]);
         }
         //println!("");
     }
 
-    for i in 0..256 {
-        for j in 0..256 {
-            mat2[[i, mat[[i, j]] as usize]] = j as u8;
+    for _i in 0..256 {
+        for _j in 0..256 {
+            mat2[[_i, mat[[_i, _j]] as usize]] = _j as u8;
         }
     }
     /*
@@ -391,7 +380,7 @@ fn main() {
     let seed = rng2.gen::<u64>();
     sk = random_shuffule(sk, 256, seed);
     let sk2 = sk.clone();
-    let mut x:Vec<u8>;
+    
 
     println!("何か入力を");
     std::io::stdin().read_line(&mut data).ok();
@@ -408,24 +397,27 @@ fn main() {
     f.write(&gg.as_bytes()).unwrap();
     println!("{:?}",f);
 
-    x=vec![0];
-    for i in 0..32{
-    dd[i]=f[i];
+
+
+    for _i in 0..32{
+    dd[_i]=f[_i];
     }
-    let mut tmp:&[u8]=&f[32..f.len()];
+    let tmp:&[u8]=&f[32..f.len()];
     //for i in 0..f.len()-32{
-        x=tmp.to_vec();
+    let x:Vec<u8>=tmp.to_vec();
     //}
     println!("{:?}",x);
     let v=x.clone();
-    let mut w=hmac(String::from_utf8(x).unwrap(),seed2);
+    let w=hmac(String::from_utf8(x).unwrap(),seed2);
     println!("decd={:?}",w);
     //exit(1);
-    let mut z:String=String::from_utf8(v).unwrap();
+    let z:String=String::from_utf8(v).unwrap();
     println!(" ");
-    
+
     // encoded above
     let l = dec(&z, &sk2, &mat2);
 
     println!("back to origin: {}", l);
+
+    exit(1);
 }
