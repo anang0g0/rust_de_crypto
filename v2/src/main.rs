d567a04067a1ad84eb0ee770260a6d26de825e67
anang0g0 <fumumue@protonmail.com>
2022-06-21 16:50:25 +0900

    serpentine

--- v2/src/main.rs
+++ v2/src/main.rs
@@ -946,7 +946,7 @@ let Sbox =arr2(&[
         for _i in 0..256 {
             //j{
             //j {
-            buf[_i]=bite(buf[_i] as usize,_k%8);
+            //buf[_i]=bite(buf[_i] as usize,_k%8);
             buf[_i] ^= ((w[_i % 60] % 256) as u8) ^ be[nk[_i % 32] as usize];
             //=
             //
@@ -1083,7 +1083,7 @@ fn dec(encoded: &String, a: &[u8; 256], mat: &Array2<u8>, seed2: &[u8]) -> Strin
             decoded[i] = INV_S_BOX[(((decoded[i] % 16) + (decoded[i] >> 4) * 16) as usize)];
             //decoded[i]^=fg[decoded[i] as usize];
             decoded[i] ^= ((w[i % 60] % 256) as u8) ^ be[ee[i % 32] as usize]; //
-            decoded[i]=u2(decoded[i],7-(j%8));
+            //decoded[i]=u2(decoded[i],7-(j%8));
             //S_BOX[(be[ee[i%32] as usize]%16 + ((be[ee[i%32] as usize]>>4)*16)) as usize];
             //gf[mlt(oinv(be[i%32] as u16),fg[decoded[i] as usize] as u16) as usize];
         }
