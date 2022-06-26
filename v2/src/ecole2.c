0cb4a64a5bb7df86bba35a0032b7ce5324e0f1e2
anang0g0 <fumumue@protonmail.com>
2022-06-26 16:57:55 +0900

    reguler

--- v2/src/ecole2.c
+++ v2/src/ecole2.c
@@ -31,7 +31,6 @@ static const unsigned int normal[15] = {
     0b110111,
     0b1100001,
     0b11000001,
-    // 0b100011011, // aes (irreducible but not primitive)
     // 0b110101001, // normal
     0b100011101, // sage
     // 0b100011011,
@@ -47,24 +46,6 @@ static const unsigned int normal[15] = {
     0b1100000000000001,
     0b11010000000010001
 };
-
-/*************************************************
- * seki(unsigned int a, unsigned int b)
- * -- returns product of a & b, maybe.
- */
-unsigned int seki(unsigned int a, unsigned int b)
-{
-    unsigned int prod = 0;    // seki means product ?
-
-    while (a > 0) {
-        if (a & 1) {
-            prod = prod ^ b;
-        }
-        b <<= 1;
-        a >>= 1;
-    }
-    return prod;
-}
 
 /*************************************************
  * n2bitnum(unsigned int num)
@@ -91,79 +72,6 @@ typedef struct {
     unsigned int remainder;    // あまり（余）
 } JOZAN;
 
-static JOZAN jozan(unsigned int p, unsigned int d)
-{
-    JOZAN ans = { p, p };         // { 0, p }; ではないだろうか？
-    const int bitD = n2bitnum(d);
-
-    if (n2bitnum(p) < bitD)
-        return ans;
-
-    unsigned int qt = 0;   // しょうになる
-    unsigned int rm = p;   // あまりになる
-
-    while (1) {
-        int bitRM = n2bitnum(rm);
-        if (bitRM < bitD) break;
-
-        int shift = bitRM - bitD;
-        if (shift < 0) break;
-
-        unsigned int y = (shift > 0) ? d << shift : d;
-        if (n2bitnum(y) == bitRM) {
-            qt = qt + (1 << shift);
-            rm = rm ^ y;
-        }
-    }
-    /* 結果出た */
-    ans.quotient  = qt;    // しょう
-    ans.remainder = rm;    // あまり
-    return ans;
-}
-
-/*************************************************
- * f2quot(int p, int d)
- * -- calcullate F_2 quot 
- * return value :  p / d;
- */
-unsigned int f2quot(unsigned int p, unsigned int d)
-{
-    JOZAN t = jozan(p, d);
-    return t.quotient;
-}
-
-/*************************************************
- * f2_mod(unsigned int p, unsigned int d)
- * -- calcullate F_2 mod, p % d 
- */
-unsigned int f2_mod(unsigned int p, unsigned int d)
-{
-    JOZAN t = jozan(p, d);
-    return t.remainder;
-}
-
-/*************************************************
- * inv(unsigned short a, unsigned short n)
- * -- invert integer
- */
-unsigned short inv(unsigned short a, unsigned short n)
-{
-    unsigned short d = n;
-    unsigned short x = 0;
-    unsigned short s = 1;
-
-    while (a > 0) {
-        JOZAN kotae = jozan(d, a);
-        unsigned short q = kotae.quotient; 
-        unsigned short r = kotae.remainder;
-        unsigned short t = x ^ seki(q, s);
-
-        d = a, x = s;
-        a = r, s = t;
-    }
-
-    return f2_mod((x ^ n), f2quot(n, d));
-}
 
 /*************************************************
  * outputTbl(char *tblName, unsigned int *pTbl, int size)
